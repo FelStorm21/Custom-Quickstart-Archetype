d6dd42401cf8b816bce8d2c6e391131ca87988e0
Benjamin Zalla <bzalla10@gmail.com>
2019-01-09 12:11:20 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+This is a custom Maven quickstart archetype that includes automatic jar shading. 
---
+++ src/main/resources/archetype-resources/src/main/java/Main.java
@@ -0,0 +1,9 @@
+package ${groupId}.${artifactId};
+
+public class Main {
+	
+	public static void main(String[] args) {
+		
+	}
+	
+}
